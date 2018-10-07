c7d9570a8d704cdf9c3009102b9f024976a7f47c
suryl008 <suresh.pounraj@gmail.com>
2018-10-07 10:28:51 -0400

    wireframe

---
+++ Crowe/Crowe/Controllers/DetailsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace Crowe.Controllers
+{
+    public class DetailsController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Crowe/GetHeartbeat")]
+        public string GetStatusMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ Crowe/Crowe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Crowe.WebApiApplication" Language="C#" %>
